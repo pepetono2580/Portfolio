3274dd1a1c734d64ad47e7f63ced2d6044819014
jose rasgado <jrasgado01@hotmail.com>
2024-04-11 23:42:36 -0600

    Add description below columns

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.set_page_config()
 
 col1, col2 = st.columns(2)
 
@@ -10,7 +11,11 @@ with col2:
     st.title("José A. Rasgado")
     content = """
     Hello! I'm José and I am a Python developer, I graduated in 2021 as a Software Engineer from Anáhuac Mayab University.
-    Since then, I've worked in multiple companies using different tools, such as No-Code technologies like Bubble and RPA
-    platforms such as Automation Anywhere, which led me to learn Python and create the projects presented in this portfolio.
+    Since then, I've worked in multiple companies using different tools, such as No-Code technologies for building dynamic 
+    websites and web-apps with Bubble, and RPA platforms such as Automation Anywhere, which led me to learn Python and 
+    create the projects presented in this portfolio.
     """
     st.info(content)
+
+content2 = """Below you can find me some of the apps I have built in Python. Feel free to contact me"""
+st.write(content2)
